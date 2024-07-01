b5cdd1d7a090da26633c26f5d00992266af392ba
assumpta <mbithiassumpta@gmail.com>
2024-07-01 12:34:17 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(name=["Guadalupe", "Ollie", "Aki"], event="surprise") {
+    const array = []
+    for (let i = 0; i < name.length; i++) {
+        array.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    }
+    return array;
+}
+
+let x=0;
+function countDown(number) {
+    while (number > 0) {
+        console.log(number);
+        number -= 1;
+    }
+    console.log(number);
+}
